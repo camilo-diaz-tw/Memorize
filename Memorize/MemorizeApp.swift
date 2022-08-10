917a523a713ae50fb37586b7b126dec3a6beb29b
Camilo Diaz <camilo.j@thoughtworks.com>
2022-08-10 17:18:39 +1000

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Camilo Diaz on 10/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
