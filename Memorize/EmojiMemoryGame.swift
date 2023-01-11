84909e3c1deaf4828c40b686ed6c7ec58c8c75fe
Camilo Diaz <camilo.j@thoughtworks.com>
2023-01-12 10:43:49 +1100

    lecture 5

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,38 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Camilo Diaz on 21/8/2022.
+//
+
+import SwiftUI
+
+// This is my viewModel
+class EmojiMemoryGame: ObservableObject {
+  typealias Card = MemoryGame<String>.Card
+    
+    private static let emojis = ["✈️", "🚂", "🚁", "🚜", "🚗", "🚠",
+                  "🚌", "🚎", "🚐", "🚑", "🚒", "🚟",
+                  "🚓", "🚕", "🚙", "🛻", "🚚", "🚋",
+                  "🛰", "🚀", "🛸", "🛶", "⛵️", "🚢" ]
+    
+    private static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    
+    // @Published property wrapper means that any change to the below var will trigger a rerender
+    @Published private var model = createMemoryGame()
+        
+    var cards: Array<Card> {
+        model.cards
+    }
+  
+  // MARK: - Intents
+  
+  func choose(_ card: Card) {
+    objectWillChange.send() // any time the ui needs redrawing
+    model.choose(card)
+  }
+}
